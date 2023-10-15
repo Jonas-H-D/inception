c19f0f09f5963f875016bede044dfa35052c6919
Jonas Hermon-duc <jhermon-@student.42lausanne.ch>
2023-10-15 15:04:21 +0200

    admin user login working in wordpress, port is 443

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,7 +1,12 @@
+# Modifier le fichier www.conf pour que ca fonctionne en local comme demande
 target="/etc/php7/php-fpm.d/www.conf"
 
+# Le fichier www.conf est relatif a php-fpm (necessaire communication avec le serveur)
 grep -E "listen = 127.0.0.1" $target > /dev/null 2>&1
+#$? contient le code de retour de la dernière opération
+#si le retour d erreur et egale a 0 alors
 if [ $? -eq 0 ]; then
+	#remplacer la premiere partie par la deuxieme
 	sed -i "s|.*listen = 127.0.0.1.*|listen = 9000|g" $target
 	echo "env[MARIADB_HOST] = \$MARIADB_HOST" >> $target
 	echo "env[MARIADB_USER] = \$MARIADB_USER" >> $target
@@ -9,12 +14,13 @@ if [ $? -eq 0 ]; then
 	echo "env[MARIADB_DB] = \$MARIADB_DB" >> $target
 fi
 
+# si tu ne trouve pas "fichier" alors
 if [ ! -f "wp-config.php" ]; then
 	cp /config/wp-config ./wp-config.php
 
 	# laisser un temps d attente sinon les etapes suivantes seront skippees
 	# La connexion a la base de donnees se fait dans ce temps
-	sleep 15
+	sleep 5
 
 	# Configuration du site wordpress
 	#wp commande wordpress
@@ -30,7 +36,7 @@ if [ ! -f "wp-config.php" ]; then
 	wp-cli.phar user create $WP_USER $WP_USER_EMAIL --role=editor --user_pass=$WP_USER_PWD
 
 	# Creation d'un article pour l'example (change number)
-	wp-cli.phar post generate --count=2 --post_title="Jonas's Inception Post"
+	wp-cli.phar post generate --count=2 --post_title="Jonas Inception Post"
 fi
 
 # faire tourner wordpress mais aussi pour que le container keep running
